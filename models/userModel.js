97de4729fe6700923563fa14c75b05ac839264a1
Reham Eid <jecekaoni@gmail.com>
2024-11-03 22:22:28 +0200

    done "loginWithGoogle"

--- models/userModel.js
+++ models/userModel.js
@@ -1,31 +1,35 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const userSchema= new mongoose.Schema({
-userName:{
-type:String,
-required:true,
-tirm:true
-},
-email:{
-    type:String,
-    required:true,
-    tirm:true,
-    unique:true
-},
-password:{
-    type:String,
-    required:true,
-},
-phone:{
-    type:Number,
-    required:true,
-    tirm:true
-},
-role:{
-    type:String,
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    tirm: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    tirm: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    tirm: true,
+  },
+  role: {
+    type: String,
+  },
+  token: String,
+  provider: {
+    type: String,
+    enum: ["GOOGLE", "System"],
+    default: "System",
+  },
+});
 
-}
-
-})
-
-module.exports=mongoose.model('User',userSchema)
+module.exports = mongoose.model("User", userSchema);
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,11 @@
-const express=require('express')
-const router =express.Router();
-const userController= require('../controllers/userController')
-const authMiddleware = require('../middelware/verifyToken')
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const authMiddleware = require("../middelware/GenerateAndVerifyToken");
 
-router.post('/register',userController.register)
-router.post('/login',authMiddleware,userController.login)
+router.post("/register", userController.register);
+router.post("/login", authMiddleware, userController.login);
 
-module.exports=router
+router.post("/login/google",userController.loginWithGoogle );
+
+module.exports = { userRouter: router };
