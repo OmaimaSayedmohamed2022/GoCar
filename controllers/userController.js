a82b23e448407a85ce8cd20725ccfd6bec2f3f7f
omaima sayed <116375938+OmaimaSayedmohamed2022@users.noreply.github.com>
2024-11-04 00:52:03 +0200

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,49 +1,53 @@
-const User = require("../models/userModel");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { verifyGoogleToken } = require("../utils/google.strategy.js");
-const userModel = require("../models/userModel.js");
-const { status } = require("../utils/system.roles.js");
-const { generateToken } = require("../middelware/GenerateAndVerifyToken.js");
+import User from'../models/userModel.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
-const register = async (req, res) => {
-  try {
-    const { userName, email, password, confirmPassword, phone, role } =
-      req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({
-      userName,
-      email,
-      phone,
-      role,
-      password: hashedPassword,
-    });
-    await newUser.save();
-    res.status(201).json({ message: "User registered successfully", newUser });
-  } catch (error) {
-    console.log("new user register error");
-    res
-      .status(404)
-      .json({ message: "new user register error", error: error.message });
-  }
+
+export const register = async (req, res) => {
+    try {
+        const { userName, email, password, confirmPassword, phone, role } = req.body;
+        
+        if (password !== confirmPassword) {
+            return res.status(400).json({ message: 'Passwords do not match' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ userName, email, phone, role, password: hashedPassword });
+        await newUser.save();
+        
+        res.status(201).json({ message: 'User registered successfully', newUser });
+    } catch (error) {
+        console.log("new user register error", error);
+        res.status(404).json({ message: "new user register error", error: error.message });
+    }
 };
 
-const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
 
-    await user.save();
-    return res.status(201).send({ message: "user login successfully" });
-  } catch (error) {
-    console.log("error in login", error);
-    return res
-      .status(500)
-      .send({ message: "error in login ", error: error.message });
-  }
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+
+        const token = jwt.sign({ userId: user._id }, process.env.KEY_TOKEN, { expiresIn: '1h' });
+        
+        res.status(200).json({ message: 'User login successfully', token });
+    } catch (error) {
+        console.log('Error in login:', error);
+        return res.status(500).send({ message: 'Error in login', error: error.message });
+    }
 };
 
-const loginWithGoogle = async (req, res) => {
+export const loginWithGoogle = async (req, res) => {
   async (req, res) => {
     // const { idToken } = req.body;
     const idToken = req.headers["id-token"];
@@ -105,8 +109,8 @@ const loginWithGoogle = async (req, res) => {
     }
   };
 };
-module.exports = {
-  register,
-  login,
-  loginWithGoogle,
-};
+// module.exports = {
+//   register,
+//   login,
+//   loginWithGoogle,
+// };
