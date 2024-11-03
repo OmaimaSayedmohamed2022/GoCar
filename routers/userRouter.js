46daed6b3c4d56840f7f249476a7a4965c29711c
omaima sayed <116375938+OmaimaSayedmohamed2022@users.noreply.github.com>
2024-11-04 00:49:41 +0200

    Update userRouter.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,29 @@
-const express = require("express");
+import express from 'express';
+import { register, login } from '../controllers/userController.js';
+import { registerValidation,loginValidation } from '../middelware/validation.js';
+import { validationResult } from 'express-validator';
+
 const router = express.Router();
-const userController = require("../controllers/userController");
-const authMiddleware = require("../middelware/GenerateAndVerifyToken");
 
-router.post("/register", userController.register);
-router.post("/login", authMiddleware, userController.login);
+// Registration router
+router.post('/register', registerValidation, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    register(req, res);
+});
 
+// Login router
+router.post('/login', loginValidation, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    login(req, res);
+});
+
+// login with google router
 router.post("/login/google",userController.loginWithGoogle );
 
-module.exports = { userRouter: router };
+export default router;
