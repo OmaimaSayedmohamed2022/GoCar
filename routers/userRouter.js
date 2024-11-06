7589623ba431d74060d5b9175ebcb30e9ac258a5
omaima sayed <116375938+OmaimaSayedmohamed2022@users.noreply.github.com>
2024-11-06 21:28:20 +0200

    Update userRouter.js
    add forget ,verify code and reset password routers

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -10,6 +10,10 @@ import {
   loginValidation,
 } from "../middelware/validation.js";
 
+import {validatorHandlerMiddleware} from '../middelware/validatorHandlerMiddleware.js'
+import { userAuth } from '../middelware/userMiddleware.js';
+import { requestPasswordReset, verifyCode, resetPassword } from '../controllers/forgetPassword.js';
+
 const router = express.Router();
 
 // Registration router
@@ -23,5 +27,14 @@ router.post("/login/google", loginWithOAuth);
 
 // sign-up with google router
 router.post("/sign-up/google", signupWithOAuth);
+
+// request for reset password router
+router.post('/forget-password',userAuth, requestPasswordReset);
+
+// verify code router
+router.post('/verify-code', verifyCode);
+
+//reset new password router
+router.post('/reset-password',userAuth,loginValidation, resetPassword);
 
 export default router;
